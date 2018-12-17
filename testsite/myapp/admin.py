cc8038383e4101b8536999f5fcbe84325a827f8b
pithers <bsmith83@mail.csuchico.edu>
2018-12-16 23:54:09 -0800

    fixed importing issues

--- testsite/myapp/admin.py
+++ testsite/myapp/admin.py
@@ -2,7 +2,7 @@
 
 from django.contrib import admin
 
-from .models import *
+from . import models 
 
 # Register your models here.
-admin.site.register(SuggestionModel)
+admin.site.register(models.SuggestionModel)
