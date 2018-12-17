fe05e57465e437c19ba88175e1bb3d21827105b7
pithers <bsmith83@mail.csuchico.edu>
2018-12-16 23:54:09 -0800

    fixed importing issues

--- testsite/myapp/models.py
+++ testsite/myapp/models.py
@@ -3,7 +3,7 @@
 from django.db import models
 
 class SuggestionModel(models.Model):
-    suggestion = models.CharField(max_length = 240)
+    suggestion = models.CharField(max_length=240)
 
     def __str__(self):
         return self.suggestion
--- testsite/myapp/views.py
+++ testsite/myapp/views.py
@@ -3,10 +3,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . import models
 
 #Default view
 def index(request):
-    context = {'title':'Title'}
+    suggestions = models.SuggestionModel.objects.all()
+    context = {
+        'title':'Title',
+        "suggestions":suggestions
+    }
     return render(request, 'index.html', context=context)
 
 #View provided page, day, and year. ex: site/page/2003/2
