62da24ccd05a7b558a3632396ad58d5ac0446b81
pithers <bsmith83@mail.csuchico.edu>
2018-12-26 01:40:07 -0800

    Added barebones commenting system

--- testsite/myapp/tests.py
+++ testsite/myapp/tests.py
@@ -8,9 +8,9 @@ from .models import SuggestionModel
 #This will all be handled in a fake database
 class SuggestionTestCase(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username='bob',email='bob@bob.com',password='randyrandy')
-        SuggestionModel.objects.create(suggestion="lion",author=self.user)
-        SuggestionModel.objects.create(suggestion="cat",author=self.user)
+        self.user = User.objects.create_user(username='bob', email='bob@bob.com', password='randyrandy')
+        SuggestionModel.objects.create(suggestion="lion", author=self.user)
+        SuggestionModel.objects.create(suggestion="cat", author=self.user)
 
     def test_suggestions_to_string(self):
         lion = SuggestionModel.objects.get(suggestion="lion")
