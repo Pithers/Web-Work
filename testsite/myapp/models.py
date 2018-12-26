e58fee00cfd26f2e81ced67b33d1af4856006e79
pithers <bsmith83@mail.csuchico.edu>
2018-12-25 22:54:34 -0800

    Author field added to database for suggestions

--- testsite/myapp/models.py
+++ testsite/myapp/models.py
@@ -1,9 +1,12 @@
 #myapp/models.py
+#Consider making a profile model that inherits from User so each user can have a profile
 
 from django.db import models
+from django.contrib.auth.models import User
 
 class SuggestionModel(models.Model):
     suggestion = models.CharField(max_length=240)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.suggestion
+        return "%s authored by %s" % (self.suggestion, self.author)
