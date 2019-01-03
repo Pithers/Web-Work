3d8b406ea735b0c3a6bc0ef6d5288906513c1ad7
pithers <bsmith83@mail.csuchico.edu>
2019-01-03 03:24:07 -0800

    Dark-mode switch now remembers through session storage

--- testsite/myapp/static/js/app.js
+++ testsite/myapp/static/js/app.js
@@ -1,10 +1,26 @@
 $(document).foundation()
 
+//Grab session data and act accordingly
+function loadSession() {
+    var color_mode = sessionStorage.getItem('color-mode');
+    if(color_mode !== null) {
+        document.querySelector('body').className = color_mode;
+
+        if(color_mode == 'dark-mode'){
+          document.getElementById('light-switch').checked = true;
+        } else if (color_mode == 'light-mode') {
+          document.getElementById('light-switch').checked = false;
+        }
+    }
+}
+
 //Site theme darkmode toggle
 function themeUpdate(element) {
     if(element.checked) {
-        document.querySelector('body').className = "night-mode";
+        document.querySelector('body').className = 'dark-mode';
+        sessionStorage.setItem('color-mode', 'dark-mode')
     } else {
-        document.querySelector('body').className = "day-mode";
+        document.querySelector('body').className = 'light-mode';
+        sessionStorage.setItem('color-mode', 'light-mode')
     }
 }
