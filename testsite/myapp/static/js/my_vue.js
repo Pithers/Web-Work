51b8c03a49cc5e769ad1dc360f851e93128ef41f
pithers <bsmith83@mail.csuchico.edu>
2019-01-12 04:43:30 -0800

    Changed databast object labelled 'suggestions' to 'posts', changed default user model to custom user model, moved index to 'posts' page so that index can serve a different function.

--- testsite/myapp/static/js/my_vue.js
+++ testsite/myapp/static/js/my_vue.js
@@ -13,62 +13,62 @@ Vue.directive('foundation', {
   }*/
 })
 
-//Vue object that fetches our suggestions/comment model from the database
+//Vue object that fetches our post/comment model from the database
 var fetch_app = new Vue({
   delimiters: ['[[',']]'],
-  el: '#fetch_suggestion',
+  el: '#fetch_post',
   data () {
     return {
-      suggestions_list: [],
+      posts_list: [],
       sort_type: 'newest',
     }
   },
 
   mounted: function() {
-    //Grab suggestions list and then periodically update every 10 seconds
-    this.getSuggestionsList();
-    this.timer = setInterval(this.getSuggestionsList, 10000);
+    //Grab posts list and then periodically update every 10 seconds
+    this.getPostsList();
+    this.timer = setInterval(this.getPostsList, 10000);
   },
 
   methods: {
-    //Sorts the suggestion_list object by id, suggestion, author, and num of comments
+    //Sorts the post_list object by id, post, author, and num of comments
     sortedList: function() {
       switch(this.sort_type) {
         case 'newest':
-          return this.suggestions_list.slice().sort(function(a,b){return b.id-a.id});
+          return this.posts_list.slice().sort(function(a,b){return b.id-a.id});
           break;
         case 'oldest':
-          return this.suggestions_list.slice().sort(function(a,b){return a.id-b.id});
+          return this.posts_list.slice().sort(function(a,b){return a.id-b.id});
           break;
         case 'a_to_z':
-          return this.suggestions_list.slice().sort(function(a,b){return a.suggestion.localeCompare(b.suggestion)});
+          return this.posts_list.slice().sort(function(a,b){return a.post.localeCompare(b.post)});
           break;
         case 'z_to_a':
-          return this.suggestions_list.slice().sort(function(a,b){return b.suggestion.localeCompare(a.suggestion)});
+          return this.posts_list.slice().sort(function(a,b){return b.post.localeCompare(a.post)});
           break;
         case 'author_a':
-          return this.suggestions_list.slice().sort(function(a,b){return a.author.localeCompare(b.author)});
+          return this.posts_list.slice().sort(function(a,b){return a.author.localeCompare(b.author)});
           break;
         case 'author_z':
-          return this.suggestions_list.slice().sort(function(a,b){return b.author.localeCompare(a.author)});
+          return this.posts_list.slice().sort(function(a,b){return b.author.localeCompare(a.author)});
           break;
         case 'most':
-          return this.suggestions_list.slice().sort(function(a,b){return b.comments.length-a.comments.length});
+          return this.posts_list.slice().sort(function(a,b){return b.comments.length-a.comments.length});
           break;
         case 'least':
-          return this.suggestions_list.slice().sort(function(a,b){return a.comments.length-b.comments.length});
+          return this.posts_list.slice().sort(function(a,b){return a.comments.length-b.comments.length});
           break
         default:
-          return this.suggestions_list.slice().sort(function(a,b){return a.id-b.id});
+          return this.posts_list.slice().sort(function(a,b){return a.id-b.id});
       }
     },
 
-    getSuggestionsList: function() {
+    getPostsList: function() {
       axios
       //Access our own API to get a json object
-        .get('/suggestions/')
+        .get('/rest_posts/')
       //Make sure to grab the response data, not the response itself
-        .then(response => (this.suggestions_list = response.data.suggestions))
+        .then(response => (this.posts_list = response.data.posts))
     },
 
     cancelAutoUpdate: function() {
--- testsite/myapp/urls.py
+++ testsite/myapp/urls.py
@@ -1,14 +1,16 @@
 #myapp/urls.py
 
 from django.urls import path
+from django.conf.urls import url
 from django.contrib.auth import views as adminviews
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('suggestions/', views.rest_suggestion),
+    path('', views.index_view),
+    path('posts/', views.post_view),
+    path('rest_posts/', views.rest_post),
     path('register/', views.register),
-    path('comment/<int:suggestion_id>/', views.comment_view),
+    path('comment/<int:post_id>/', views.comment_view),
     path('login/', adminviews.LoginView.as_view()),
     path('logout/', views.logout_view),
     path('webgl/<slug:name>/', views.webgl_view),
