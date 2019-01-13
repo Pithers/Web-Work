7806ee5d2d162b53a36caa65d14850a2fd948655
pithers <bsmith83@mail.csuchico.edu>
2019-01-13 04:57:14 -0800

    Implemented color palette picker. Also reconfigured how the css color variables work.

--- testsite/myapp/static/js/app.js
+++ testsite/myapp/static/js/app.js
@@ -1,26 +1,54 @@
-$(document).foundation()
+$(document).foundation();
 
-//Grab session data and act accordingly
+function light_mode() {
+    document.documentElement.style.setProperty("--color-bg", "#e5e4d3");
+    document.documentElement.style.setProperty("--color-text", "#241a22");
+    document.documentElement.style.setProperty("--color-text-ui", "#241a22");
+    document.documentElement.style.setProperty("--color-text-invert", "#e5e4d3");
+    document.documentElement.style.setProperty("--color-text-highlight", "#e5e4d3");
+    document.documentElement.style.setProperty("--color-base", "#817b7b");
+    document.documentElement.style.setProperty("--color-accent", "#667075");
+    document.documentElement.style.setProperty("--color-tertiary", "#666666");
+    document.documentElement.style.setProperty("--color-border", "#838378");
+    document.documentElement.style.setProperty("--color-border-accent", "#727267");
+    document.documentElement.style.setProperty("--color-border-transp", "#e5e4d399");
+    document.documentElement.style.setProperty("--color-drop-shadow", "#00000033");
+}
+
+function dark_mode() {
+    document.documentElement.style.setProperty("--color-bg", "#24252f");
+    document.documentElement.style.setProperty("--color-text", "#eaeced");
+    document.documentElement.style.setProperty("--color-text-ui", "#eaeced");
+    document.documentElement.style.setProperty("--color-text-invert", "#24252f");
+    document.documentElement.style.setProperty("--color-text-highlight", "#24252f");
+    document.documentElement.style.setProperty("--color-base", "#7f838e");
+    document.documentElement.style.setProperty("--color-accent", "#7dacc4");
+    document.documentElement.style.setProperty("--color-tertiary", "#666666");
+    document.documentElement.style.setProperty("--color-border", "#826c73");
+    document.documentElement.style.setProperty("--color-border-accent", "#715d62");
+    document.documentElement.style.setProperty("--color-border-transp", "#24252f99");
+    document.documentElement.style.setProperty("--color-drop-shadow", "#ffffff33");
+}
+
+//Grab session data and set color-mode and checkbox accordingly
 function loadSession() {
     var color_mode = sessionStorage.getItem('color-mode');
-    if(color_mode !== null) {
-        document.querySelector('body').className = color_mode;
-
-        if(color_mode == 'dark-mode'){
-          document.getElementById('light-switch').checked = true;
-        } else if (color_mode == 'light-mode') {
-          document.getElementById('light-switch').checked = false;
-        }
+    if(color_mode == 'dark-mode'){
+        dark_mode();
+        document.getElementById('light-switch').checked = true;
+    } else if (color_mode == 'light-mode') {
+        light_mode();
+        document.getElementById('light-switch').checked = false;
     }
 }
 
-//Site theme darkmode toggle
+//Site theme darkmode toggle, set root css variables accordingly
 function themeUpdate(element) {
     if(element.checked) {
-        document.querySelector('body').className = 'dark-mode';
-        sessionStorage.setItem('color-mode', 'dark-mode')
+        dark_mode();
+        sessionStorage.setItem('color-mode', 'dark-mode');
     } else {
-        document.querySelector('body').className = 'light-mode';
-        sessionStorage.setItem('color-mode', 'light-mode')
+        light_mode();
+        sessionStorage.setItem('color-mode', 'light-mode');
     }
 }
---
+++ testsite/myapp/static/js/color_mode.js
@@ -0,0 +1,53 @@
+//myapp/static/js/color_mode.js
+
+//Color elements
+var color_bg = document.getElementById("color-bg");
+var color_text = document.getElementById("color-text");
+var color_text_ui = document.getElementById("color-text-ui");
+var color_text_invert = document.getElementById("color-text-invert");
+var color_text_highlight = document.getElementById("color-text-highlight");
+var color_base = document.getElementById("color-base");
+var color_accent = document.getElementById("color-accent");
+var color_tertiary = document.getElementById("color-tertiary");
+var color_border = document.getElementById("color-border");
+var color_border_accent = document.getElementById("color-border-accent");
+var color_border_transp = document.getElementById("color-border-transp");
+var color_drop_shadow = document.getElementById("color-drop-shadow");
+
+color_bg.onchange = function() {
+  document.documentElement.style.setProperty("--color-bg", "#" + color_bg.value);
+};
+color_text.onchange = function() {
+  document.documentElement.style.setProperty("--color-text", "#" + color_text.value);
+};
+color_text_ui.onchange = function() {
+  document.documentElement.style.setProperty("--color-text-ui", "#" + color_text_ui.value);
+};
+color_text_invert.onchange = function() {
+  document.documentElement.style.setProperty("--color-text-invert", "#" + color_text_invert.value);
+};
+color_text_highlight.onchange = function() {
+  document.documentElement.style.setProperty("--color-text-highlight", "#" + color_text_highlight.value);
+};
+color_base.onchange = function() {
+  document.documentElement.style.setProperty("--color-base", "#" + color_base.value);
+};
+color_accent.onchange = function() {
+  document.documentElement.style.setProperty("--color-accent", "#" + color_accent.value);
+};
+color_tertiary.onchange = function() {
+  document.documentElement.style.setProperty("--color-tertiary", "#" + color_tertiary.value);
+};
+color_border.onchange = function() {
+  document.documentElement.style.setProperty("--color-border", "#" + color_border.value);
+};
+color_border_accent.onchange = function() {
+  document.documentElement.style.setProperty("--color-border-accent", "#" + color_border_accent.value);
+};
+color_border_transp.onchange = function() {
+  document.documentElement.style.setProperty("--color-border-transp", "#" + color_border_transp.value);
+};
+color_drop_shadow.onchange = function() {
+  document.documentElement.style.setProperty("--color-drop-shadow", "#" + color_drop_shadow.value);
+};
+
