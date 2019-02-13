92e5d67a79ee862e6fa0d0518e1d09c1b5eea393
pithers <bsmith83@mail.csuchico.edu>
2019-02-13 01:20:51 -0800

    HTTPS now successfully works

--- testsite/testsite/settings.py
+++ testsite/testsite/settings.py
@@ -34,10 +34,10 @@ LOGIN_REDIRECT_URL = '/'
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 # Network settings
-#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-#SECURE_SSL_REDIRECT = True
-#SESSION_COOKIE_SECURE = True
-#CSRF_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # Application definition
 
@@ -141,8 +141,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-#STATIC_ROOT = os.path.join(BASE_DIR, "/deploy_static/")
 STATIC_ROOT = BASE_DIR + "/static/"
 STATIC_URL = '/static/'
 SASS_PROCESSOR_ROOT = STATIC_ROOT
