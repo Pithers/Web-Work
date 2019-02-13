374b16a5bed4683c9a85c26b37c07a18d89e8183
pithers <bsmith83@mail.csuchico.edu>
2019-02-13 01:20:51 -0800

    HTTPS now successfully works

---
+++ cleanup_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+shopt -s expand_aliases
+alias docker='docker.exe'
+
+echo "Docker cleanup..."
+echo "Stopping docker processes..."
+docker stop $(docker ps -aq) 2>/dev/null
+docker rm $(docker ps -aq) 2>/dev/null
+docker rmi $(docker images -q) 2>/dev/null
+echo "Docker processes and images removed/cleaned"
