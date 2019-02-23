8f2fbb3966ff296e7e3eb3ef582bc0536c075663
pithers <bsmith83@mail.csuchico.edu>
2019-02-23 02:58:54 -0800

    Fixed preferences.js file to generate javascript withough any inline js or inline styles.

--- testsite/testsite/settings.py
+++ testsite/testsite/settings.py
@@ -86,7 +86,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'csp_nonce.middleware.CSPNonceMiddleware',
+                'csp_nonce.context_processors.nonce',
             ],
         },
     },
