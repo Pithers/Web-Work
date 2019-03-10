da5d90941cb461366338238db789a5d4e2a80912
pithers <bsmith83@mail.csuchico.edu>
2019-03-10 01:55:26 -0800

    Created deployment script for first time server init. It should install everything needed and set up certificates for the production server.

---
+++ deploy_init.sh
@@ -0,0 +1,48 @@
+#Deploy_init.sh
+#This shell script installs all the needed programs to run the server.
+#The script then builds the server from pither's gitlab image,
+#creates the database via manage.py makemigrations and migrate
+#then creates a superuser as defined in db_init.sh.
+
+#Before running this script you should:
+#1. Change the SECRET_KEY in production-compose.yml
+#2. Change the superuser credentials in db_init.sh
+
+#NOTE: This only needs to be run once on the new server. 
+#After running this just use docker-compose -f production-compose.yml web up
+
+#Update apt-get
+sudo apt-get update
+
+#============Install Docker============
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+#============Install Docker-Compose============
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+#============Install Nginx============
+sudo apt-get install nginx
+
+#============Install Certbot============
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install certbot
+
+#Build server, run server db_init script, run server, and create certificates
+#certbot will create a system level cron job at /etc/cron.d/certbot which auto renews the certificate
+docker-compose -f production-compose.yml run web ./db_init.sh
+docker-compose -f production-compose.yml up 
+sudo certbot certonly --webroot -w ./certs-data/ -d pithers.org -d www.pithers.org
--- readme.md
+++ readme.md
@@ -2,6 +2,7 @@
 
 A small repository of test work done for web development
 
+Please run the db_init.sh from inside the docker env before using. Edit the db_init.sh file with the credentials you want.
 
 Master:
 [![coverage report](https://gitlab.com/pithers/Web-Work/badges/master/coverage.svg)](https://gitlab.com/pithers/Web-Work/commits/master)
