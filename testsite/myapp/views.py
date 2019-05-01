a6c467949c0def573aec1081af86de6593a58d98
pithers <bsmith83@mail.csuchico.edu>
2019-04-30 20:31:01 -0700

    Trying to fix python lint errors with error handling.

--- testsite/myapp/views.py
+++ testsite/myapp/views.py
@@ -270,13 +270,13 @@ def index_view(request):
 
 # error_views
 def handler404(request):
-  return render(request, '404.html', status=404)
+    return render(request, '404.html', status=404)
 
 def handler500(request):
-  return render(request, '500.html', status=500)
+    return render(request, '500.html', status=500)
 
 def handler403(request):
-  return render(request, '403.html', status=403)
+    return render(request, '403.html', status=403)
 
 def handler400(request):
-  return render(request, '400.html', status=400)
+    return render(request, '400.html', status=400)
