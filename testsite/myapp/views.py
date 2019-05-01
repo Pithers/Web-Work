e833b799918168815fb5dc376b90e5eea60fe924
pithers <bsmith83@mail.csuchico.edu>
2019-04-30 20:11:42 -0700

    Added barebones error handler views to site.

--- testsite/myapp/views.py
+++ testsite/myapp/views.py
@@ -16,6 +16,7 @@
 # about_view
 # music_view
 # index_view
+# error_views
 
 # Imports
 from django.shortcuts import render, HttpResponse, redirect
@@ -266,3 +267,16 @@ def index_view(request):
         return redirect('/')
     color_scheme_form = forms.ColorSchemeForm()
     return render(request, 'index.html', {'color_scheme_form': color_scheme_form})
+
+# error_views
+def handler404(request):
+  return render(request, '404.html', status=404)
+
+def handler500(request):
+  return render(request, '500.html', status=500)
+
+def handler403(request):
+  return render(request, '403.html', status=403)
+
+def handler400(request):
+  return render(request, '400.html', status=400)
