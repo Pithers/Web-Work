074415d5507ff315968bf05ba4d8aeaf23efcd4e
pithers <bsmith83@mail.csuchico.edu>
2019-05-13 06:28:43 -0700

    React barebones working together with youtube API to generate a list of playlists based on searched youtube user.

--- testsite/myapp/static/js/react_app.js
+++ testsite/myapp/static/js/react_app.js
@@ -1,13 +1,21 @@
-//Filename: react_app.js
+//Filename: react_app.js/x
 //Author: Brandon Smith
 //File Description:
+//This is a youtube playlist randomizer app
+//Set up multiple playlists in youtube and this will
+//Shuffle videos between them
+//Notes:
+//As this is a JSX app, here will be an attempt to contain the
+//JS, HTML, and CSS styling all in a single file. There have been
+//arguments made about the benefit of this; so this is kind of an
+//experiment in how it reads.
 //References:
 //Contents:
 //##
 const APIKey = "AIzaSyCHDk3UdiZ5MEXlFKRwCdhzDGDPi2dD4x0";
 const baseURL = "https://www.googleapis.com/youtube/v3/"; //Get the specified username
 
-function youtubeUserIdPromise(username) {
+function getYoutubeUserId(username) {
   return new Promise(function (resolve, reject) {
     $.get(baseURL + 'channels', {
       part: "id",
@@ -19,7 +27,7 @@ function youtubeUserIdPromise(username) {
   });
 }
 
-function youtubePlaylistsPromise(id) {
+function getYoutubePlaylists(id) {
   return new Promise(function (resolve, reject) {
     $.get(baseURL + 'playlists', {
       part: "snippet",
@@ -42,39 +50,121 @@ function youtubePlaylistVideosPromise(listId) {
       resolve(data);
     });
   });
-} //Retreive playlist from name
-
-
-function getUserPlaylists(username) {
-  youtubeUserIdPromise(username).then(function (user_id) {
-    youtubePlaylistsPromise(user_id).then(function (data) {
-      data.forEach(function (element) {
-        console.log(element.snippet.title); //console.log(element.id);
-      });
-    });
-  });
 }
 
 function getPlaylistVideos(listId) {
   youtubePlaylistVideosPromise(listId).then(function (data) {
     console.log(data.items[0].snippet.title);
   });
+} //Search For User
+//getUserPlaylists('misterpithers').then();
+//Display Playlists from User
+//Select Playlists from User
+//getPlaylistVideos('PLH0PzuhXpJp2N1XZN7ikHF8UqDcbCbY5q');
+//Cancelable promise
+
+/*const makeCancelable = (promise) => {
+  let hasCanceled_ = false;
+
+  const wrappedPromise = new Promise((resolve, reject) => {
+    promise.then(
+      val => hasCanceled_ ? reject({isCanceled: true}) : resolve(val),
+      error => hasCanceled_ ? reject({isCanceled: true}) : reject(error)
+    );
+  });
+
+  return {
+    promise: wrappedPromise,
+    cancel() {
+      hasCanceled_ = true;
+    },
+  };
+};
+
+const cancelablePromise = makeCancelable(
+  //Search for typed user and get all playlists
+  getYoutubeUserId(this.state.term).then((user_id) => {
+    getYoutubePlaylists(user_id).then((data) => {
+      data.forEach(function(element) {
+        console.log(element.snippet.title);
+        console.log(element.id);
+      });
+      this.setState({
+        playlists: ['a'],
+      });
+    });
+  })
+);*/
+//Main class. App goes here
+
+
+class PlaylistRandomizer extends React.Component {
+  render() {
+    return React.createElement(Playlist, null);
+  }
+
 }
 
-getUserPlaylists('misterpithers');
-getPlaylistVideos('PLH0PzuhXpJp2N1XZN7ikHF8UqDcbCbY5q');
+class Playlist extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      playlists: []
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      term: event.target.value
+    });
+    $.get(baseURL + 'channels', {
+      part: "id",
+      forUsername: this.state.term,
+      key: APIKey
+    }, channel => {
+      $.get(baseURL + 'playlists', {
+        part: "snippet",
+        channelId: channel.items[0].id,
+        maxResults: 25,
+        key: APIKey
+      }, list => {
+        this.setState({
+          playlists: list.items
+        });
+      });
+    });
+  }
+
+  render() {
+    const items = this.state.playlists.map((element, i) => {
+      return React.createElement("li", {
+        key: i
+      }, element.snippet.title);
+    });
+    return React.createElement("div", null, items, React.createElement("label", {
+      htmlFor: "user-search"
+    }, "Input Username"), React.createElement("input", {
+      onChange: this.handleChange,
+      name: "user-search",
+      type: "text",
+      value: this.state.term
+    }));
+  }
+
+}
 
 class Clock extends React.Component {
   render() {
     return React.createElement("div", null, React.createElement("h1", null, "Hello, world!"), React.createElement("h2", null, "It is ", this.props.date.toLocaleTimeString(), "."));
   }
 
-}
+} //Create DOM
+
 
 function tick() {
-  ReactDOM.render(React.createElement(Clock, {
-    date: new Date()
-  }), document.getElementById('react-block'));
+  ReactDOM.render(React.createElement(PlaylistRandomizer, null), document.getElementById('react-block'));
 }
 
 setInterval(tick, 1000);
--- testsite/myapp/static/jsx/react_app.jsx
+++ testsite/myapp/static/jsx/react_app.jsx
@@ -1,7 +1,16 @@
-//Filename: react_app.js
+//Filename: react_app.js/x
 //Author: Brandon Smith
 
 //File Description:
+//This is a youtube playlist randomizer app
+//Set up multiple playlists in youtube and this will
+//Shuffle videos between them
+
+//Notes:
+//As this is a JSX app, here will be an attempt to contain the
+//JS, HTML, and CSS styling all in a single file. There have been
+//arguments made about the benefit of this; so this is kind of an
+//experiment in how it reads.
 
 //References:
 
@@ -10,34 +19,6 @@
 
 const APIKey = "AIzaSyCHDk3UdiZ5MEXlFKRwCdhzDGDPi2dD4x0";
 const baseURL = "https://www.googleapis.com/youtube/v3/";
-
-//Get the specified username
-function youtubeUserIdPromise(username) {
-  return new Promise(function(resolve, reject) {
-    $.get(baseURL + 'channels', {
-      part: "id",
-      forUsername: username,
-      key: APIKey },
-      function(data) {
-        resolve(data.items[0].id);
-      }
-    );
-  });
-}
-
-function youtubePlaylistsPromise(id) {
-  return new Promise(function(resolve, reject) {
-    $.get(baseURL + 'playlists', {
-      part: "snippet",
-      channelId: id,
-      maxResults: 25,
-      key: APIKey },
-      function(data) {
-        resolve(data.items);
-      }
-    );
-  });
-}
 
 function youtubePlaylistVideosPromise(listId) {
   return new Promise(function(resolve, reject) {
@@ -52,41 +33,79 @@ function youtubePlaylistVideosPromise(listId) {
   });
 }
 
-//Retreive playlist from name
-function getUserPlaylists(username) {
-  youtubeUserIdPromise(username).then(function(user_id) {
-    youtubePlaylistsPromise(user_id).then(function(data) {
-      data.forEach(function(element) {
-        console.log(element.snippet.title);
-        //console.log(element.id);
-      });
-    });
-  });
-}
-
 function getPlaylistVideos(listId) {
   youtubePlaylistVideosPromise(listId).then(function(data) {
     console.log(data.items[0].snippet.title);
   });
 }
 
-getUserPlaylists('misterpithers');
-getPlaylistVideos('PLH0PzuhXpJp2N1XZN7ikHF8UqDcbCbY5q');
-
-class Clock extends React.Component {
+//Main class. App goes here
+class PlaylistRandomizer extends React.Component {
   render() {
     return (
+      <Playlist />
+    );
+  }
+}
+
+//App for generating playlists
+class Playlist extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      playlists: [],
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      term: event.target.value,
+    });
+
+    //NEXT: Implement error handling here
+    //Find channel of user searched
+    $.get(baseURL + 'channels', {
+      part: "id",
+      forUsername: this.state.term,
+      key: APIKey },
+      (channel) => {
+        //Get playlists from user listed
+        $.get(baseURL + 'playlists', {
+          part: "snippet",
+          channelId: channel.items[0].id,
+          maxResults: 25,
+          key: APIKey },
+          (list) => {
+            this.setState({
+              playlists: list.items,
+            })
+          }
+        );
+      }
+    );
+  }
+
+  render() {
+    const items = this.state.playlists.map((element, i) => {
+      return <li key={i}>{ element.snippet.title }</li>
+    });
+    return (
       <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      { items }
+      <label htmlFor="user-search">Input Username</label>
+      <input onChange={this.handleChange} name='user-search' type="text" value={this.state.term}/>
       </div>
     );
   }
 }
 
+//Create DOM
 function tick() {
   ReactDOM.render(
-    <Clock date={new Date()} />,
+    <PlaylistRandomizer />,
     document.getElementById('react-block')
   );
 }
